2e63323ccefcd544984e1e0b283217c5ff538101
hms <2355790530@qq.com>
2022-08-19 12:06:29 +0800

    获取zipkin。，trackid和spanid

--- consumer/src/main/java/consumer/controllers/ConsumerController.java
+++ consumer/src/main/java/consumer/controllers/ConsumerController.java
@@ -1,10 +1,17 @@
 package consumer.controllers;
 
+
+import brave.Tracer;
+import brave.propagation.TraceContext;
 import consumer.FeignClient.ProviderClient;
+
+import feign.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +28,16 @@ public class ConsumerController {
     @Value("${server.port}")
     String port;
 
+    @Resource
+    private Tracer tracer;
+
     @GetMapping("/hi-xiaoFeiFeign")
     public String Test() {
+
+        //获取链路id
+        TraceContext a = tracer.currentSpan().context();
+        System.out.println("TrackId" + tracer.currentSpan().context().traceIdString());
+        System.out.println("SpanId" + tracer.currentSpan().context().spanIdString());
         //log.info("1成为大师傅士大夫");
         return providerClient.hi("Test");
     }
--- provider/src/main/java/provider/controllers/TestController.java
+++ provider/src/main/java/provider/controllers/TestController.java
@@ -1,11 +1,14 @@
 package provider.controllers;
 
 
+import brave.Tracer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/providerTest")
@@ -14,9 +17,15 @@ public class TestController {
 
     @Value("${server.port}")
     String port;
+    @Resource
+    private Tracer tracer;
 
     @GetMapping("/hi")
     public String hi(@RequestParam(value = "name", defaultValue = "forezp", required = false) String name) {
+
+        //获取链路id
+        System.out.println("TrackId" + tracer.currentSpan().context().traceIdString());
+        System.out.println("SpanId" + tracer.currentSpan().context().spanIdString());
         return "providerHello " + name + ", i'm provider ,my port:" + port;
     }
 
